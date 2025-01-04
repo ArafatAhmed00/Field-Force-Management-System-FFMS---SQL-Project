abbcfd423a4dacaeed3c5a85ac33b2660b752679
ArafatAhmed00 <162756730+ArafatAhmed00@users.noreply.github.com>
2025-01-04 03:43:12 -0500

    Advanced queries with joins and subqueries.

---
+++ SQLQueryProject2.sql
@@ -0,0 +1,9 @@
+select * from Trainees
+--update Trainees set name=' Kamal' where Id=1
+
+--update Trainees set name='Kamal' where Id=2
+
+select * from Trainees where Id=2
+
+select * from Trainees where Trim(name)='Kamal'
+
